197ab2d8b609c3477a4f34d31336914701d628a5
dmitriydenisov <dmitriyd2009@yandex.ru>
2022-04-30 17:00:00 +0400

    меню на странице доставки

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,6 +1,6 @@
 if (document.querySelector(".clubs__slider")) {
   const swiper = new Swiper(".clubs__slider", {
-    slidesPerView: 5,
+    slidesPerView: 4,
     spaceBetween: 39,
     loop: true,
     autoplay: {
@@ -16,7 +16,7 @@ if (document.querySelector(".discounts__slider")) {
     spaceBetween: 60,
     loop: true,
     autoplay: {
-      delay: 2500,
+      delay: 1500,
       disableOnInteraction: false,
     },
   });
@@ -34,6 +34,7 @@ if (document.querySelector(".restaurant__slider")) {
   });
 }
 
+//табы на странице ресторана
 const tabItems = Array.from(document.querySelectorAll(".restaurant-menu__btn"));
 const contentItem = Array.from(
   document.querySelectorAll(".tabs-content__item")
@@ -61,14 +62,68 @@ const checkoutTabs = (item, index) => {
 
 tabItems.forEach(checkoutTabs);
 
-// for (const tab of tabItems) {
-//   checkoutTabs();
-// }
+//изменение количесвткатовара в карточке
+const counters1 = document.querySelectorAll("[data-counter]");
+for (const count of counters1) {
+  const catrMinusBtns = count.querySelector(".cart-minus");
+  const catrPlusBtns = count.querySelector(".cart-plus");
+  const counts = count.querySelector(".count");
 
+  let countontent = parseInt(count.textContent);
+
+  function plus(event) {
+    event.preventDefault();
+    ++countontent;
+    counts.textContent = countontent;
+  }
+
+  function minus(event) {
+    event.preventDefault();
+    if (countontent > 1) {
+      --countontent;
+      counts.textContent = countontent;
+    }
+  }
+  catrMinusBtns.addEventListener("click", minus);
+  catrPlusBtns.addEventListener("click", plus);
+}
+
+const counters = document.querySelectorAll("[data-counter]");
+
+//меню на странице доставки
 const menu = document.querySelector(".btn-block__item-more");
 
 if (menu) {
-  menu.addEventListener("click", function () {
+  // menu.addEventListener("click", function () {
+  //   this.classList.add("btn-block__item-more--active");
+  // });
+  !menu.addEventListener("click", function () {
     this.classList.toggle("btn-block__item-more--active");
   });
+}
+
+//модальное окно на странице доставки
+const carts = document.querySelectorAll(".cart__img");
+const modal = document.querySelector(".delivery-popup");
+
+for (const cart of carts) {
+  cart.addEventListener("click", () => {
+    modal.classList.add("modal-active");
+  });
+}
+const closeModal = document.querySelector(".delivery-popup__close");
+if (closeModal) {
+  closeModal.addEventListener("click", () => {
+    modal.classList.remove("modal-active");
+  });
+}
+
+//модальное окно в корзине
+if (document.querySelector(".basket-products__btn")) {
+  const basketBtnn = document
+    .querySelector(".basket-products__btn")
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      modal.classList.add("modal-active");
+    });
 }
